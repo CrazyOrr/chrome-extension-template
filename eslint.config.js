facbbd0d00d2d2fc6a3b2882d16fa0ed9fcd0063
Lei Wang <wanglei021212@gmail.com>
2025-12-07 14:27:19 +0800

    include gitignore file in eslint configuration to specify ignoring files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig([
-  globalIgnores(['dist/']),
+  includeIgnoreFile(fileURLToPath(new URL('.gitignore', import.meta.url))),
   {
     languageOptions: {
       globals: {
